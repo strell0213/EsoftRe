6d5f8100de2fe2d259af301da3014722bdf2e08e
strell0213 <57249104+strell0213@users.noreply.github.com>
2022-10-30 21:48:42 +0500

    fc

---
+++ AgencyRE/AgencyRE/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AgencyRE
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            updateRE();
+        }
+        public void updateRE() { 
+            var r = App.ADB.RealEstates.Select(x => x.TypeREs.NameType + "\n"+x.Vcode+"\nКол-во комнат: "+x.QuaRooms+". Этаж:"+x.Flat+"\nАдрес и координаты: "+x.AddressRE+" "+x.coordinates).ToList();
+            RealEstatesViews.ItemsSource = r;
+        }
+        private void MainLogIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (LogInGrid.Visibility == Visibility.Hidden)
+            {
+                LogInGrid.Visibility = Visibility.Visible;
+            }
+            else if (LogInGrid.Visibility == Visibility.Visible)
+            {
+                LogInGrid.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            NClass.UserType = 1;
+            SignUpWindow signUpWindow = new SignUpWindow();
+            signUpWindow.Show();
+        }
+
+        private void LogInGrid_button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] FIO = FIOText.Text.Split(' ');
+            string surname = FIO[0];
+            string name = FIO[1];
+            string patronomic = FIO[2];
+
+
+            var r = App.ADB.Realtors.Where(c => c.Surname == surname && c.Namee == name && c.Patronomic == patronomic).FirstOrDefault();
+            if (r == null)
+            {
+                var w = App.ADB.Clients.Where(s => s.Surname == surname && s.Namee == name && s.Patronomic == patronomic).FirstOrDefault();
+                if (w == null)
+                {
+                    MessageBox.Show("dsa");
+                }
+                else
+                {
+                    //клиент
+                    MainLogIn.Visibility = Visibility.Hidden;
+                    LogInGrid.Visibility = Visibility.Hidden;
+                    UserGrid.Visibility = Visibility.Visible;
+                    FIOLabel.Content = surname + " " + name + " " + patronomic + "\nКлиент";
+                }
+            }
+            else
+            {
+                //риэлтор
+                MainLogIn.Visibility = Visibility.Hidden;
+                LogInGrid.Visibility = Visibility.Hidden;
+                UserGrid.Visibility = Visibility.Visible;
+                AddRealtorButton.Visibility = Visibility.Visible;
+                REAdd.Visibility = Visibility.Visible;
+                FIOLabel.Content = surname + " " + name + " " + patronomic+ "\nРиэлтор";
+            }
+        }
+
+        private void AddRealtorButton_Click(object sender, RoutedEventArgs e)
+        {
+            NClass.UserType = 2;
+            SignUpWindow signUpWindow = new SignUpWindow();
+            signUpWindow.Show();
+        }
+
+        private void REAdd_Click(object sender, RoutedEventArgs e)
+        {
+            NClass.UserType = 3;
+            SignUpWindow signUpWindow = new SignUpWindow();
+            signUpWindow.Show();
+        }
+    }
+}
---
+++ AgencyRE/AgencyRE/SignUpWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AgencyRE
+{
+    /// <summary>
+    /// Логика взаимодействия для SignUpWindow.xaml
+    /// </summary>
+    public partial class SignUpWindow : Window
+    {
+
+        public SignUpWindow()
+        {
+            InitializeComponent();
+            if (NClass.UserType == 1 || NClass.UserType == 2)
+            {
+                RealtorsAndClientsGrid.Visibility = Visibility.Visible;
+            }
+            else if (NClass.UserType == 3) { 
+                REGrid.Visibility = Visibility.Visible;
+                var type = App.ADB.TypeREs.Select(x => x.NameType).ToList();
+                TypeText.ItemsSource = type;
+                QuaRoomsext.Items.Add("1");
+                QuaRoomsext.Items.Add("2");
+                QuaRoomsext.Items.Add("3");
+                QuaRoomsext.Items.Add("4");
+                QuaRoomsext.Items.Add("5");
+
+                QuaFlatText.Items.Add("1");
+                QuaFlatText.Items.Add("2");
+                QuaFlatText.Items.Add("3");
+
+                for (int i = 1; i <= 25; i++) {
+                    FlatText.Items.Add(i.ToString());
+                }
+            }
+        }
+
+        private void LogInGrid_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (NClass.UserType == 1)
+            {
+
+                int CountForDone = 0;
+                //проверка имени
+                if (NameText.Text != null)
+                {
+                    CountForDone++;
+                }
+                if (CountForDone == 1)
+                {
+                    Clients clients = new Clients()
+                    {
+                        Namee = NameText.Text,
+                        Surname = SurnameText.Text,
+                        Patronomic = PatronomicText.Text,
+                        Age = 18,
+                        PhoneNumber = PhoneText.Text,
+                        Email = EmailText.Text
+                    };
+                    App.ADB.Clients.Add(clients);
+                    App.ADB.SaveChanges();
+                    MessageBox.Show("Успешно", "esoft", MessageBoxButton.OK, MessageBoxImage.Information);
+                    this.Close();
+                    NClass.UserType = 0;
+                }
+                else
+                {
+                    MessageBox.Show("Не корректно введены данные", "esoft", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else if (NClass.UserType == 2)
+            {
+                int CountForDone = 0;
+                //проверка имени
+                if (NameText.Text != null)
+                {
+                    CountForDone++;
+                }
+                if (CountForDone == 1)
+                {
+                    Realtors realtors = new Realtors()
+                    {
+                        Namee = NameText.Text,
+                        Surname = SurnameText.Text,
+                        Patronomic = PatronomicText.Text
+                    };
+                    App.ADB.Realtors.Add(realtors);
+                    App.ADB.SaveChanges();
+                    MessageBox.Show("Успешно", "esoft", MessageBoxButton.OK, MessageBoxImage.Information);
+                    this.Close();
+                    NClass.UserType = 0;
+                }
+                else
+                {
+                    MessageBox.Show("Не корректно введены данные", "esoft", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else if (NClass.UserType == 3) {
+                int CountForDone = 0;
+                //проверка имени
+                if (VcodeText.Text != null)
+                {
+                    CountForDone++;
+                }
+                if (CountForDone == 1)
+                {
+                    RealEstates realEstates = new RealEstates()
+                    {
+                        Vcode = Convert.ToInt32(VcodeText.Text),
+                        AddressRE = AddressText.Text,
+                        TypeRE = Convert.ToInt32(TypeText.SelectedIndex + 1),
+                        coordinates = "аа забыл",
+                        QuaRooms = Convert.ToInt32(QuaRoomsext.SelectedItem.ToString()),
+                        QuaFlats = Convert.ToInt32(QuaFlatText.SelectedItem.ToString()),
+                        Flat = Convert.ToInt32(FlatText.SelectedItem.ToString()),
+                        Squares = SqText.Text,
+                        Price = Convert.ToInt32(PriceText.Text),
+                    };
+                    App.ADB.RealEstates.Add(realEstates);
+                    App.ADB.SaveChanges();
+                    MessageBox.Show("Успешно", "esoft", MessageBoxButton.OK, MessageBoxImage.Information);
+                    this.Close();
+                    NClass.UserType = 0;
+                }
+                else
+                {
+                    MessageBox.Show("Не корректно введены данные", "esoft", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
